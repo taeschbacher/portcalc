e9969273ea24f85d1707c2c1d937162af735fb2e
Thomas Aeschbacher <thomas@thomasaeschbacher.com>
2025-12-04 11:24:36 +0100

    updated usd to chf conversion logic

--- src/data/data.py
+++ src/data/data.py
@@ -5,8 +5,6 @@ def data_preparation():
     usdchf = pd.read_csv("data/usdchf.csv", delimiter=";", index_col="Date")
     usdchf = usdchf.query('D1 == "USD1" and D0 == "M1"')
 
-    usdchf["return"] = usdchf["Value"] / usdchf["Value"].shift(1) - 1
-
     historyIndex = pd.read_csv(
         "data/historyIndex.csv", delimiter=";", index_col="Date", thousands=","
     )
@@ -28,23 +26,28 @@ def data_preparation():
     historyIndex["date"] = pd.to_datetime(historyIndex.index, format="%b %d, %Y")
     historyIndex["month"] = historyIndex["date"].dt.to_period("M")
 
-    historyIndex["usa_return"] = historyIndex["usa"] / historyIndex["usa"].shift(1) - 1
-    historyIndex["ch_return"] = historyIndex["ch"] / historyIndex["ch"].shift(1) - 1
-    historyIndex["uk_return"] = historyIndex["uk"] / historyIndex["uk"].shift(1) - 1
-    historyIndex["jp_return"] = historyIndex["jp"] / historyIndex["jp"].shift(1) - 1
-    historyIndex["emu_return"] = historyIndex["emu"] / historyIndex["emu"].shift(1) - 1
-    historyIndex["em_return"] = historyIndex["em"] / historyIndex["em"].shift(1) - 1
-
     merged = pd.merge(
         historyIndex, usdchf, how="left", on="month", suffixes=("_index", "_usdchf")
     )
 
-    merged["usa_return_chf"] = merged["usa_return"] + merged["return"]
-    merged["ch_return_chf"] = merged["ch_return"] + merged["return"]
-    merged["uk_return_chf"] = merged["uk_return"] + merged["return"]
-    merged["jp_return_chf"] = merged["jp_return"] + merged["return"]
-    merged["emu_return_chf"] = merged["emu_return"] + merged["return"]
-    merged["em_return_chf"] = merged["em_return"] + merged["return"]
+    merged["usa_return_chf"] = (merged["usa"] * merged["Value"]) / (
+        merged["usa"].shift(1) * merged["Value"].shift(1)
+    ) - 1
+    merged["ch_return_chf"] = (merged["ch"] * merged["Value"]) / (
+        merged["ch"].shift(1) * merged["Value"].shift(1)
+    ) - 1
+    merged["uk_return_chf"] = (merged["uk"] * merged["Value"]) / (
+        merged["uk"].shift(1) * merged["Value"].shift(1)
+    ) - 1
+    merged["jp_return_chf"] = (merged["jp"] * merged["Value"]) / (
+        merged["jp"].shift(1) * merged["Value"].shift(1)
+    ) - 1
+    merged["emu_return_chf"] = (merged["emu"] * merged["Value"]) / (
+        merged["emu"].shift(1) * merged["Value"].shift(1)
+    ) - 1
+    merged["em_return_chf"] = (merged["em"] * merged["Value"]) / (
+        merged["em"].shift(1) * merged["Value"].shift(1)
+    ) - 1
 
     returns_chf = merged[
         [
